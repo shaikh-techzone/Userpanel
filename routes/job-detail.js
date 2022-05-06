d517bf6096e9ec6b6c0f4ebd85de297e46c01542
shaikh-techzone <raeeshouseproduction@gmail.com>
2022-05-06 19:05:52 +0500

    Added Job detail Page

--- routes/job-detail.js
+++ routes/job-detail.js
@@ -1,9 +1,20 @@
 const express = require("express");
 
+const JOBS = require("../schemas/job");
+
 const router = express.Router();
 
-router.get("/job-detail", (req, res) => {
-  res.render("job-detail", { title: "Job Detail" });
+router.get("/job-detail/:id", async (req, res) => {
+  let id, job;
+  id = req.params.id;
+  await JOBS.findById(id)
+    .then((result) => {
+      job = result;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.render("job-detail", { title: "Job Detail", job });
 });
 
 module.exports = router;
