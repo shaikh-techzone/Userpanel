d003af20ebbd01d57d56a79f06ed279c1de277a3
shaikh-techzone <raeeshouseproduction@gmail.com>
2022-05-06 18:40:15 +0500

    showing job data on UI

--- routes/home.js
+++ routes/home.js
@@ -1,9 +1,19 @@
-const express = require('express')
+const express = require("express");
+
+const JOBS = require("../schemas/job");
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('index', { title: "Home" })
-})
+router.get("/", async (req, res) => {
+  let jobs;
+  await JOBS.find()
+    .then((result) => {
+      jobs = result;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.render("index", { title: "Home", jobs });
+});
 
 module.exports = router;
