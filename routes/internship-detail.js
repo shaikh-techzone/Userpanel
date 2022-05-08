042c144742f5308682692d6268151659813e3db7
UsamabSadiq <usamabsadiq@gmail.com>
2022-05-08 08:25:22 -0700

    created detail pages for internship and scholarship

--- routes/internship-detail.js
+++ routes/internship-detail.js
@@ -1,9 +1,20 @@
 const express = require("express");
 
+const INTERN = require("../schemas/internship");
+
 const router = express.Router();
 
-router.get("/internship-detail", (req, res) => {
-  res.render("internship-detail", { title: "Internship Detail" });
+router.get("/internship-detail/:id", async (req, res) => {
+  let id, internships;
+  id = req.params.id;
+  await INTERN.findById(id)
+    .then((result) => {
+      internships = result;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.render("internship-detail", { title: "Internships Detail", internships });
 });
 
 module.exports = router;
--- routes/scholarship-detail.js
+++ routes/scholarship-detail.js
@@ -1,9 +1,20 @@
 const express = require("express");
 
+const SCHOLAR = require("../schemas/scholarship");
+
 const router = express.Router();
 
-router.get("/scholarship-detail", (req, res) => {
-  res.render("scholarship-detail", { title: "Scholarship Detail" });
+router.get("/scholarship-detail/:id", async (req, res) => {
+  let id, scholarships;
+  id = req.params.id;
+  await SCHOLAR.findById(id)
+    .then((result) => {
+      scholarships = result;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.render("scholarship-detail", { title: "Scholarship Detail", scholarships });
 });
 
 module.exports = router;
