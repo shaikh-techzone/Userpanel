97f411876a27270588a46435e788e6596e15acb4
shaikh-techzone <raeeshouseproduction@gmail.com>
2022-05-10 00:26:02 +0500

    College & University Detail list Pages modified + College Schema

--- routes/college-detail.js
+++ routes/college-detail.js
@@ -1,9 +1,18 @@
 const express = require("express");
-
+const COLLEGE = require("../schemas/college");
 const router = express.Router();
 
-router.get("/college-detail", (req, res) => {
-  res.render("college-detail", { title: "College Detail" });
+router.get("/college-detail/:id", async (req, res) => {
+  let id, college;
+  id = req.params.id;
+  await COLLEGE.findById(id)
+    .then((result) => {
+      college = result;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.render("college-detail", { title: "College Detail", college });
 });
 
 module.exports = router;
--- routes/university-detail.js
+++ routes/university-detail.js
@@ -1,9 +1,18 @@
 const express = require("express");
-
+const UNI = require("../schemas/university");
 const router = express.Router();
 
-router.get("/university-detail", (req, res) => {
-  res.render("university-detail", { title: "University Detail" });
+router.get("/university-detail/:id", async (req, res) => {
+  let id, university;
+  id = req.params.id;
+  await UNI.findById(id)
+    .then((result) => {
+      university = result;
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  res.render("university-detail", { title: "University Detail", university });
 });
 
 module.exports = router;
